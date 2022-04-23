d95fd3eaef280cb9ea38288818caa85336fe058e
Tomasz Jarko <tomasz.jarko@gmail.com>
2022-04-23 14:41:24 +0200

    Adding quadratic_equation solver program

---
+++ python/delta.py
@@ -0,0 +1,3 @@
+
+def delta(a, b, c):
+    return pow(b, 2)-(4*a*c)
---
+++ python/quadratic_equation.py
@@ -0,0 +1,22 @@
+from cmath import sqrt
+from delta import delta
+
+print("ax^2 + bx + c = 0")
+a=int(input("a: "))
+b=int(input("b: "))
+c=int(input("c: "))
+
+print("Δ = ", delta(a, b, c))
+
+if delta(a, b, c) < 0:
+    print("Δ is lower than 0, equation has no solutions")
+if delta(a, b, c) == 0:
+    x = (-1*b)/(2*a)
+    print("x = ", x)
+else:
+    print("Formula has 2 roots:")
+    x1 = ((-b)-(sqrt(delta(a, b, c))))/(2*a)
+    x2 = ((-b)+(sqrt(delta(a, b, c))))/(2*a)
+    
+    print("x1 = ", x1)
+    print("x2 = ", x2)
